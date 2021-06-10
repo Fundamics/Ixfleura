fbb9a7a64063d5ef1ffc54c147d556777dd28c75
Aarav Navani <aarav.navani@outlook.com>
2021-06-10 19:25:10 +0530

    Added mod or admin check

---
+++ Ixfleura/Commands/Checks/RequireModOrAdminAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Disqord;
+using Disqord.Bot;
+using Qmmands;
+
+namespace Ixfleura.Commands.Checks
+{
+    public class RequireModOrAdminAttribute : DiscordCheckAttribute
+    {
+        private readonly Snowflake _modId = new Snowflake(828662023666663473);
+        private readonly Snowflake _adminId = new Snowflake(828662023666663473);
+        
+        public override ValueTask<CheckResult> CheckAsync(DiscordCommandContext context)
+        {
+            if (context.Author is not IMember member)
+                return Failure("You need to be inside a guild");
+
+            if (!(member.RoleIds.Contains(_modId) || member.RoleIds.Contains(_adminId)))
+                return Failure("You do not have the necessary permissions to do this command");
+
+            return Success();
+        }
+    }
+}
