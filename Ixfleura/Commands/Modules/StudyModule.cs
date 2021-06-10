d2ff532a778673dd9f0d9743958a624dfbb2bee8
Aarav Navani <aarav.navani@outlook.com>
2021-06-10 12:38:49 +0530

    Added study module

---
+++ Ixfleura/Commands/Modules/StudyModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Disqord.Bot;
+using Disqord.Rest;
+using Ixfleura.Commands.Checks;
+using Microsoft.Extensions.Configuration;
+using Qmmands;
+
+namespace Ixfleura.Commands.Modules
+{
+    public class StudyModule : DiscordGuildModuleBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public StudyModule(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        [Command("studying")]
+        [Description("Mark yourself as studying. Use again to remove the studying role")]
+        [RequireFundamics]
+        public async Task<DiscordCommandResult> StudyingRoleAsync()
+        {
+            var studyingRoleId = Convert.ToUInt64(_configuration["fundamics:studying_id"]);
+
+            if (Context.Author.RoleIds.Contains(studyingRoleId))
+            {
+                await Context.Author.RevokeRoleAsync(studyingRoleId);
+                return Response("You are no longer studying!");
+            }
+
+            await Context.Author.GrantRoleAsync(studyingRoleId);
+            return Response($"{Context.Author.Name}, you are now studying");
+        }
+    }
+}
