688a9a1ea946b029fcd0347a416246c791a9caae
Aarav Navani <aarav.navani@outlook.com>
2021-06-10 12:38:38 +0530

    Added Require Fundamics check

---
+++ Ixfleura/Commands/Checks/RequireFundamicsAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Disqord.Bot;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Qmmands;
+
+namespace Ixfleura.Commands.Checks
+{
+    public class RequireFundamicsAttribute : DiscordCheckAttribute
+    {
+        public RequireFundamicsAttribute()
+        { }
+        
+        public override ValueTask<CheckResult> CheckAsync(DiscordCommandContext context)
+        {
+            var configuration = context.Services.GetRequiredService<IConfiguration>();
+
+            var guildId = Convert.ToUInt64(configuration["fundamics:guild_id"]);
+            
+            if (context.GuildId is null)
+                return Failure("You can only execute this command inside the Fundamics guild!");
+            
+            if (context.GuildId != guildId)
+                return Failure("You can only execute this command inside the Fundamics guild!");
+
+            return Success();
+        }
+    }
+}
