11cfaaff5659c7a4b28c4b83803223a9c66bac7f
Aarav Navani <aarav.navani@outlook.com>
2021-06-11 17:35:57 +0530

    Added colors

---
+++ Ixfleura/Common/Types/IxColors.cs
@@ -0,0 +1,15 @@
+﻿using Disqord;
+
+namespace Ixfleura.Common.Types
+{
+    public static class IxColors
+    {
+        public static readonly Color BanColor = new Color(221, 94, 83);
+        
+        public static readonly Color UnbanColor = new Color(83, 221, 163);
+
+        public static readonly Color WarnColor = new Color(255, 250, 119);
+
+        public static readonly Color KickColor = new Color(26, 155, 226);
+    }
+}
