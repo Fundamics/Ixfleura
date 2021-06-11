49214fa74aad35598459ffbd70c673119d6d105c
Aarav Navani <aarav.navani@outlook.com>
2021-06-11 17:36:07 +0530

    Added enum extensions

---
+++ Ixfleura/Common/Extensions/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Disqord;
+using Ixfleura.Common.Types;
+
+namespace Ixfleura.Common.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static Color GetModLogColor(this ModLogType modLogType)
+        {
+            return modLogType switch
+            {
+                ModLogType.Ban => IxColors.BanColor,
+                ModLogType.Unban => IxColors.UnbanColor,
+                ModLogType.Kick => IxColors.KickColor,
+                ModLogType.Warn => IxColors.WarnColor,
+                _ => Color.White
+            };
+        }
+    }
+}
