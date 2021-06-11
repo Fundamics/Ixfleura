47b74e106d100dd96eedc3818b8fe2d42077b5f0
Aarav Navani <aarav.navani@outlook.com>
2021-06-11 19:02:42 +0530

    Added database

---
+++ Ixfleura.Data/IxfleuraDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ixfleura.Data
+{
+    public class IxfleuraDbContext : DbContext
+    {
+        public IxfleuraDbContext(DbContextOptions<IxfleuraDbContext> options) : base(options)
+        { }
+    }
+}
---
+++ Ixfleura.Data/IxfleuraDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Ixfleura.Data
+{
+    public class IxfleuraDbContextFactory : IDesignTimeDbContextFactory<IxfleuraDbContext>
+    {
+        public IxfleuraDbContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("./config.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<IxfleuraDbContext>();
+            optionsBuilder.UseNpgsql(config["database:connection"])
+                .UseSnakeCaseNamingConvention();
+
+            return new IxfleuraDbContext(optionsBuilder.Options);
+        }
+    }
+}
