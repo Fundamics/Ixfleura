b0f2640f37902bdaab6e5c0bc988480ce8d16170
Aarav Navani <aarav.navani@outlook.com>
2021-06-16 19:22:03 +0530

    Added IxfleuraGlobals

---
+++ Ixfleura/Common/Globals/IxfleuraGlobals.cs
@@ -0,0 +1,16 @@
+﻿using Disqord;
+
+namespace Ixfleura.Common.Globals
+{
+    public static class IxfleuraGlobals
+    {
+        public static readonly Snowflake OwnerId = 714821968825810945;
+        public static readonly Snowflake[] ContributorIds = {218613903653863427};
+
+        public static readonly string Description = "Ixfleura (pronounced - 'eeks-fluh-ra')" +
+                                                    " is a discord bot designed to help the Fundamics community." +
+                                                    "\nIxfleura means 'night flower' or 'flower of the night'";
+
+        public static readonly string GitHubRepo = "https://github.com/Fundamics/Ixfleura";
+    }
+}
