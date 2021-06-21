e4db9911e7594a5b5a5d62c0fa143db63becb0cc
Aarav Navani <aarav.navani@outlook.com>
2021-06-21 19:30:53 +0530

    Made changes to StatusService

--- Ixfleura/Services/StatusService.cs
+++ Ixfleura/Services/StatusService.cs
@@ -16,24 +16,34 @@ namespace Ixfleura.Services
     public class StatusService : DiscordBotService
     {
         private readonly ulong _guildId;
+        private readonly int _resetAfter;
 
         public StatusService(ILogger<StatusService> logger, DiscordBotBase bot, IConfiguration configuration) : base(logger, bot)
         {
             _guildId = configuration.GetValue<ulong>("fundamics:guild_id");
+            _resetAfter = configuration.GetValue<int>("discord:status_reset");
         }
 
         /// <summary>
         /// Sets the status with the member count of Fundamics.
         /// </summary>
-        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            await Client.WaitUntilReadyAsync(cancellationToken);
+            await Client.WaitUntilReadyAsync(stoppingToken);
 
-            while (!cancellationToken.IsCancellationRequested)
+            var guild = Client.GetGuild(_guildId);
+            var memberCount = 0;
+
+            while (!stoppingToken.IsCancellationRequested)
             {
-                await Task.Delay(TimeSpan.FromMinutes(1), cancellationToken);
-                await Client.SetPresenceAsync(new LocalActivity($"ixhelp | {Client.GetGuild(_guildId).MemberCount}",
-                    ActivityType.Playing));
+                if (memberCount != guild.MemberCount)
+                {
+                    memberCount = guild.MemberCount;
+                    
+                    await Client.SetPresenceAsync(new LocalActivity($"ixhelp | {memberCount} members", ActivityType.Playing), stoppingToken);
+                }
+
+                await Task.Delay(TimeSpan.FromMinutes(_resetAfter), stoppingToken);
             }
         }
     }
